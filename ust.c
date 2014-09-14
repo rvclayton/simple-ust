110f653a3cfcf750c3c397afce078560e60cb37f
rvclayton <rvclayton@acm.org>
2014-09-14 15:51:24 -0400

    No need to print the create parameters.

--- ust.c
+++ ust.c
@@ -14,22 +14,11 @@ ust_create(thread_fun f, void * params) {
 
   thread_descriptor * new_td = td_new();
 
-  /*
-  printf("before, f = 0x%x, p = 0x%x.\n", 
-  	 (unsigned int) f, (unsigned int) params);
-  */
-
   if (!setjmp(new_td->env)) {
     new_td->f = f;
     new_td->params = params;
     return;
     }
-
-  /*
-  printf("after, f = 0x%x, p = 0x%x.\n", 
-	 (unsigned int) (running_thread->f), 
-	 (unsigned int) (running_thread->params));
-  */
 
   (running_thread->f)(running_thread->params);
 
